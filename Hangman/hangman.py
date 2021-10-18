24d4df9e30f991ead680ef34b2ecb61aa04fa04b
Vlad Varenystya <warenitsya33@gmail.com>
2021-10-18 18:11:41 +0300

    Hamngman 2nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,3 +1,9 @@
 print("HANGMAN")
 print("The game will be available soon.")
+print("Guess the word:")
+answer = input()
+if answer == python:
+    print("You survived!")
+else:
+    print("You lost!")
 
