0a871d34790ed0f5f74eee76577b20cd6e46423b
Varenytsya Vlad <warenitsya33@gmail.com>
2021-11-08 21:30:13 +0200

    TicTacToe 2-nd stage

---
+++ TicTacToe/TicTacToe.py
@@ -0,0 +1,9 @@
+def square():
+    print('Enter cells:')
+    a = list(input())
+    print('---------')
+    print('| ' + a[0] + ' ' + a[1] + ' ' + a[2] + ' |')
+    print('| ' + a[3] + ' ' + a[4] + ' ' + a[5] + ' |')
+    print('| ' + a[6] + ' ' + a[7] + ' ' + a[8] + ' |')
+    print('---------')
+square()
