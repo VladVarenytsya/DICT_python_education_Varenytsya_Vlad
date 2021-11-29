1a0143dc9d19e23c1b545122ea81b1c8fcc9e025
Varenytsya Vlad <warenitsya33@gmail.com>
2021-11-29 07:49:17 +0200

    MatrixProcissing 2-nd and 3-rd stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,19 +1,28 @@
-m_1 = m_2 = n_1 = n_2 = 0
-b = c = d = []
+m_1 = m_2 = n_1 = n_2 = s = 0
+b = c = []
+d = []
 e = []
+answer = 9
 
 
 def matrs():
     global m_1, m_2, n_1, n_2, b, c
+    print('Enter size of first matrix: ')
     m_1, n_1 = input().split(' ')
+    print('Enter first matrix:')
     b = [[int(k) for k in input().split(' ')] for t in range(int(m_1))]
+    print('Enter size of second matrix: ')
     m_2, n_2 = input().split(' ')
+    print('Enter second matrix:')       
     c = [[int(k) for k in input().split(' ')] for t in range(int(m_2))]
 
 
 def res():
+    global d
+    print('The result is:')
     for i in range(int(m_1)):
         print(' '.join(map(str, d[i])))
+    d = []
 
 
 def sum():
@@ -28,16 +37,19 @@ def sum():
                 e = []
             res()
         else:
-            print("ERROR")
+            print("The operation cannot be performed.")
     else:
-        print("ERROR")
+        print("The operation cannot be performed.")
 
 
 def multiply_const():
     global m_1, n_1, b, e, d
+    print('Enter size of matrix: ')
     m_1, n_1 = input().split(' ')
-    b = [[int(k) for k in input().split(' ')] for t in range(int(m_1))]
-    cons = int(input())
+    print('Enter matrix:')
+    b = [[float(k) for k in input().split(' ')] for t in range(int(m_1))]
+    print('Enter constant: ')
+    cons = float(input())
     for i in range(int(m_1)):
         for j in range(int(n_1)):
             e.append(b[i][j] * cons)
@@ -46,4 +58,33 @@ def multiply_const():
     res()
 
 
-multiply_const()
+def multiply_matrix():
+    global m_1, m_2, n_1, n_2, b, c, s, e, d
+    matrs()
+    if int(m_1) == int(n_2):
+        for k in range(int(m_1)):
+            for i in range(int(n_2)):
+                for j in range(int(m_2)):
+                    s += b[k][j] * c[j][i]
+                e.append(s)
+                s = 0
+            d.append(e)
+            e = []
+        res()
+    else:
+        print('The operation cannot be performed.')
+
+
+while answer != 0:
+    print('1. Add matrices\n 2. Multiply matrix by a constant\n 3. Multiply matrices\n 0. Exit\n Your choice:')
+    answer = int(input())
+    if answer == 0:
+        break
+    elif answer == 1:
+        sum()
+    elif answer == 2:
+        multiply_const()
+    elif answer == 3:
+        multiply_matrix()
+    
+
