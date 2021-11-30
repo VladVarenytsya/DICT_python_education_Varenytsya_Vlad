c9af4078289ec946b68d652efe389bd82625a4bb
Varenytsya Vlad <warenitsya33@gmail.com>
2021-11-30 18:27:42 +0200

    MatrixProcissing 4-th stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,3 +1,4 @@
+from math import ceil
 m_1 = m_2 = n_1 = n_2 = s = 0
 b = c = []
 d = []
@@ -10,11 +11,19 @@ def matrs():
     print('Enter size of first matrix: ')
     m_1, n_1 = input().split(' ')
     print('Enter first matrix:')
-    b = [[int(k) for k in input().split(' ')] for t in range(int(m_1))]
+    b = [[int(k) for k in input().split(' ')] for _ in range(int(m_1))]
     print('Enter size of second matrix: ')
     m_2, n_2 = input().split(' ')
     print('Enter second matrix:')       
-    c = [[int(k) for k in input().split(' ')] for t in range(int(m_2))]
+    c = [[int(k) for k in input().split(' ')] for _ in range(int(m_2))]
+
+
+def matr():
+    global m_1, n_1, b, e, d
+    print('Enter size of matrix: ')
+    m_1, n_1 = input().split(' ')
+    print('Enter matrix:')
+    b = [[int(k) for k in input().split(' ')] for _ in range(int(m_1))]
 
 
 def res():
@@ -23,6 +32,14 @@ def res():
     for i in range(int(m_1)):
         print(' '.join(map(str, d[i])))
     d = []
+
+
+def res_s():
+    global b
+    print('The result is:')
+    for i in range(int(m_1)):
+        print(' '.join(map(str, (b[i]))))
+    b = []
 
 
 def sum():
@@ -47,7 +64,7 @@ def multiply_const():
     print('Enter size of matrix: ')
     m_1, n_1 = input().split(' ')
     print('Enter matrix:')
-    b = [[float(k) for k in input().split(' ')] for t in range(int(m_1))]
+    b = [[float(k) for k in input().split(' ')] for _ in range(int(m_1))]
     print('Enter constant: ')
     cons = float(input())
     for i in range(int(m_1)):
@@ -75,8 +92,67 @@ def multiply_matrix():
         print('The operation cannot be performed.')
 
 
+def t_matrix_hor():
+    global m_1, n_1, b
+    matr()
+    l = int(m_1) - 1
+    f = ceil(l / 2)
+    for i in range(f):
+        for j in range(l+1):
+            b[i][j], b[l - i][j] = b[l - i][j], b[i][j]
+    res_s()
+
+
+def t_matrix_vert():
+    global m_1, n_1, b
+    matr()
+    l = int(m_1) - 1
+    f = ceil(l / 2)
+    for i in range(l+1):
+        for j in range(f):
+            b[i][j], b[i][l - j] = b[i][l - j], b[i][j]
+    res_s()
+
+
+def t_matrix_main():
+    global m_1, n_1, b
+    matr()
+    l = int(m_1)
+    for i in range(l):
+        for j in range(l):
+            if i > j:
+                b[i][j], b[j][i] = b[j][i], b[i][j]
+    res_s()
+
+
+def t_matrix_side():
+    global m_1, n_1, b
+    matr()
+    l = int(m_1) - 1
+    for i in range(l+1):
+        for j in range(l+1):
+            if i > l - j:
+                b[i][j], b[l - j][l - i] = b[l - j][l - i], b[i][j]
+    res_s()
+
+
+def transpose_matrix():
+    print('1.Main diagonal\n2.Side diagonal\n3.Vertical line\n4.Horizontal line\nYour choice: ')
+    choice = int(input())
+    if choice == 1:
+        t_matrix_main()
+    elif choice == 2:
+        t_matrix_side()
+    elif choice == 3:
+        t_matrix_vert()
+    elif choice == 4:
+        t_matrix_hor()
+    else:
+        print('The operation cannot be performed.')
+
+
 while answer != 0:
-    print('1. Add matrices\n 2. Multiply matrix by a constant\n 3. Multiply matrices\n 0. Exit\n Your choice:')
+    print('1.Add matrices\n2.Multiply matrix by a constant\n3.Multiply matrices\n4.Transpose matrix\n0.Exit\nYour choice:')
     answer = int(input())
     if answer == 0:
         break
@@ -86,5 +162,5 @@ while answer != 0:
         multiply_const()
     elif answer == 3:
         multiply_matrix()
-    
-
+    elif answer == 4:
+        transpose_matrix()
