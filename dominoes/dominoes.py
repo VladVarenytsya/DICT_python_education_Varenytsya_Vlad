e6ce4a4c0cd6541c7c55b97f7b30c9ad75001869
Varenytsya Vlad <warenitsya33@gmail.com>
2022-02-11 16:55:04 +0200

    Dominoes 3-rd stage

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -6,9 +6,11 @@ res = []
 snake = []
 status = 0
 r_pie = 0
+pla_in = 0
+dra = []
 
 
-def start():
+def he_ha():
     for i in range(7):
         for j in range(i, 7):
             res.append([i, j])
@@ -60,20 +62,93 @@ def fenshui():
         print(pla_pie[i-1])
 
 
-def result():
-    print(70 * '=')
-    print('Stock size: ' + str(len(res)))
-    print('Computer pieces: ' + str(len(com_pie)) + '\n')
-    for i in snake:
-        print(i, end='')
-    print('\n')
-    print('Your pieces: ')
-    fenshui()
-    print('')
+def snake_v():
+    if len(snake) < 7:
+        for i in snake:
+            print(i, end='')
+    elif len(snake) > 6:
+        for i in range(6):
+            if i < 2:
+                print(snake[i], end='')
+            elif i == 2:
+                print(snake[i], end='...')
+            elif i > 2:
+                print(snake[i-6], end='')
+        
+
+def f_res():
     if status == 'computer':
         print('Status: Computer is about to make a move. Press Enter to continue...')
     elif status == 'player':
         print("Status: It's your turn to make a move. Enter your command.")
+    elif status == 'draw':
+        print("Status: The game is over. It's a draw!")
+    elif status == 'pla_w':
+        print("Status: The game is over. You won!")
+    elif status == 'com_w':
+        print("Status: The game is over. The computer won!")
+
+
+def com_step():
+    global r_pie, status
+    r_pie = choice(com_pie)
+    snake.append(r_pie)
+    com_pie.remove(r_pie)
+    status = 'player'
+
+
+def pla_step():
+    global status, pla_in, r_pie
+    if pla_in == 0:
+        r_pie = choice(res)
+        pla_pie.append(r_pie)
+        res.remove(r_pie)
+        status = 'computer'
+    elif pla_in > 0:
+        r_pie = pla_pie[pla_in-1]
+        snake.append(r_pie)
+        pla_pie.remove(r_pie)
+        status = 'computer'
+    elif pla_in < 0:
+        pla_in = abs(pla_in)
+        r_pie = pla_pie[pla_in - 1]
+        snake.insert(0, r_pie)
+        pla_pie.remove(r_pie)
+        status = 'computer'
+
+
+def draw():
+    global status, dra
+    for i in snake:
+        for j in range(2):
+            dra.append(i[j])
+        for k in range(7):
+            if dra.count(k) == 8:
+                if dra[0] == dra[-1] == k:
+                    status = 'draw'
+    dra.clear()
+
+
+def fin():
+    global status
+    if len(com_pie) == 0:
+        status = 'com_w'
+    elif len(pla_pie) == 0:
+        status = 'pla_w'
+    draw()
+
+
+def result():
+    print(70 * '=')
+    print('Stock size: ' + str(len(res)))
+    print('Computer pieces: ' + str(len(com_pie)) + '\n')
+    snake_v()
+    print('\n')
+    print('Your pieces: ')
+    fenshui()
+    print('')
+    fin()
+    f_res()
 
 
 def cho():
@@ -81,12 +156,26 @@ def cho():
         res.clear()
         com_pie.clear()
         pla_pie.clear()
-        start()
+        he_ha()
         drop_all()
         arc()
     result()
 
 
 cho()
-
+while True:
+    pla_in = input()
+    if status == 'computer':
+        com_step()
+        result()
+    elif status == 'player':
+        pla_in = int(pla_in)
+        if pla_in not in range(-len(pla_pie), len(pla_pie) + 1):
+            print('Invalid input. Please try again.')
+            continue
+        else:
+            pla_step()
+            result()
+    elif status != 'computer' or 'player':
+        break
 
